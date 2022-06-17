9efd625b6e89229f59108bb96cb1920834c1d2d3
Harshal  Pagar <72097667+harshpagar@users.noreply.github.com>
2022-06-17 11:08:10 +0530

    search

--- weather/src/components/Home.jsx
+++ weather/src/components/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import "../styles/Home.css";
 
 export const Home = () => {
     return(
     <>
-        
+            <div className='main'>
+            <div className="search">
+                    <input
+                        className="srch"
+                        type="Search  "
+                        placeholder="Search City"
+                    ></input>
+        </div>
+            </div>
     </>
     )
 }
